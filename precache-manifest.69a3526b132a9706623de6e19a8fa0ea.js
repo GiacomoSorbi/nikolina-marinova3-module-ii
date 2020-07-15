d3f219449a1754715bd4cf3d86c8603368afca66
nikolina-marinova3 <56189511+nikolina-marinova3@users.noreply.github.com>
2020-07-16 00:37:40 +0100

    Updates

---
+++ precache-manifest.69a3526b132a9706623de6e19a8fa0ea.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edbd8449bb6289ccadd51d398a1c7b9a",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/index.html"
+  },
+  {
+    "revision": "f973e71b670a0999cde8",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/css/main.0f5b38e7.chunk.css"
+  },
+  {
+    "revision": "b00783b56d9d3d005bc2",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/js/2.fa460680.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/js/2.fa460680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f973e71b670a0999cde8",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/js/main.8900dbae.chunk.js"
+  },
+  {
+    "revision": "bac96f92222a04d9eb1e",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/js/runtime-main.82546700.js"
+  },
+  {
+    "revision": "2a1a85f017954d8eca87c1885f89fbeb",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/media/defaultBcg.2a1a85f0.jpg"
+  },
+  {
+    "revision": "799444446f2bcbeb99375a00eeca37cd",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/media/details-2.79944444.jpg"
+  },
+  {
+    "revision": "1d0d9e7771ea005d3054e6218c8b60d5",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/media/details-3.1d0d9e77.jpg"
+  },
+  {
+    "revision": "4981df3d874e446259892f45651c67f1",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/media/details-4.4981df3d.jpg"
+  },
+  {
+    "revision": "71a91d9ca3f8b2ec6494b09dee565a56",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/media/flower-1.71a91d9c.jpg"
+  },
+  {
+    "revision": "f86319cd06f3875657de9cb97bf2de48",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/media/flower-10.f86319cd.jpg"
+  },
+  {
+    "revision": "5ed9a9f725e65be0a7cd9f84cbba9e74",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/media/flower-11.5ed9a9f7.jpg"
+  },
+  {
+    "revision": "192aa6c94fd41bc1f06d7e32cd0b70e8",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/media/flower-12.192aa6c9.jpg"
+  },
+  {
+    "revision": "4f43f800b760445abaf22aef0c06d01f",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/media/flower-14.4f43f800.jpg"
+  },
+  {
+    "revision": "6df5efbe67f32e4492fc6ca835766be5",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/media/flower-15.6df5efbe.jpg"
+  },
+  {
+    "revision": "816b491a2b622d8c62f407b04ff6ddcb",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/media/flower-2.816b491a.jpg"
+  },
+  {
+    "revision": "07a8a331f57c46598ad2e7d8b5d29f48",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/media/flower-3.07a8a331.jpg"
+  },
+  {
+    "revision": "6d0c1961de0c76fa8a1ba3c1df382497",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/media/flower-4.6d0c1961.jpg"
+  },
+  {
+    "revision": "89b5ad166b77b704aab35d3b0fe390bb",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/media/flower-5.89b5ad16.jpg"
+  },
+  {
+    "revision": "118fe435286001231068ccf1ed38e1a5",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/media/flower-6.118fe435.jpg"
+  },
+  {
+    "revision": "4c6d136bb3c7f2d798af799f04b168c5",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/media/flower-7.4c6d136b.jpg"
+  },
+  {
+    "revision": "e6e64a96c16119260081dceb1aa5f557",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/media/flower-8.e6e64a96.jpg"
+  },
+  {
+    "revision": "a46ec33d084cb9471bb5eab55a55145d",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/media/flower-9.a46ec33d.jpg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/GiacomoSorbi/nikolina-marinova3-module-ii/static/media/loading-arrow.e9ea7394.gif"
+  }
+]);
